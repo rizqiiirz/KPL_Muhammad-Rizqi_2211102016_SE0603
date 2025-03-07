e52c47e7016feb35dec7deb352121df7fae2b3b5
rizqiiirz <muhammadrizki66644@gamil.com>
2025-03-07 08:27:02 +0700

    First commit

---
+++ 02_pengenalan_IDE_dan_Pemograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Muhammad Rizqi"
+
+function getMyIdentity() {
+    let localVariable = 2211102016
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_pengenalan_IDE_dan_Pemograman_NodeJS/variables.js
@@ -0,0 +1,22 @@
+// var
+
+var name = "Muhammad Rizqi";
+const nim = 2211102016;
+
+console.log(name);
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim);
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`);
+
+var name = "ayam";
+
+let fullName = "Muhammad Rizqi";
+
+/*
+dsufhkudsf
+
+sjdhfjusdf
+*/
